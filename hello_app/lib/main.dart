a4ba5e416cec06d4d73b5009485e6529bd2cbc11
Administrator <Administrator@DEVSERVER>
2021-05-03 18:29:24 +0700

    inactive /active button

--- hello_app/lib/main.dart
+++ hello_app/lib/main.dart
@@ -1,43 +1,58 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
 import 'package:flutter/material.dart';
-void main()=> runApp(MyApp());
-class MyApp extends StatelessWidget {
+
+void main() {
+  runApp(MyApp());
+}
+
+class TapBoxA extends StatefulWidget {
+  TapBoxA({Key key}) : super(key: key);
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title:'Hello world demo application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  _TapBoxAState createState() => _TapBoxAState();
+}
+
+class _TapBoxAState extends State<TapBoxA> {
+  bool _active = false;
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
       ),
-      home:MyHomePage(title:'Home Page')
     );
   }
 }
-class MyHomePage extends StatelessWidget{
-  MyHomePage({Key key,this.title}) : super(key:key);
-  final String title;
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    /*
-    return Scaffold(
-      appBar:AppBar(
-        title:Text(this.title)
-      ),
-      body: Center(
-        child: Text('ภาษาไทย',style: TextStyle(fontWeight: FontWeight.bold),)
-      )
-    );*/
-    return Container(
-      decoration: BoxDecoration(color: Colors.white,),
-      padding: EdgeInsets.all(25),
-      child: Center(
-          child:Text(
-              'Hello World', style: TextStyle(
-                color: Colors.black,
-                letterSpacing: 0.5,
-                fontSize:20,
-              ),
-              textDirection: TextDirection.ltr,
-          )
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('flutter demo'),
+        ),
+        body: Center(
+          child: TapBoxA(),
+        ),
       ),
     );
   }
