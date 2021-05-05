570de0fd9466c1b1c465d7e9775c436e1030902e
Administrator <Administrator@DEVSERVER>
2021-05-05 11:39:46 +0700

    Update For TapBox State examples

--- hello_app/lib/main.dart
+++ hello_app/lib/main.dart
@@ -1,13 +1,154 @@
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyAppC());
+}
+
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetStateC createState() => _ParentWidgetStateC();
+}
+
+class _ParentWidgetStateC extends State<ParentWidgetC> {
+  bool _active = false;
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapBoxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class ParentWidgetB extends StatefulWidget {
+  @override
+  _ParentWidgetStateB createState() => _ParentWidgetStateB();
+}
+
+class _ParentWidgetStateB extends State<ParentWidgetB> {
+  bool _active = false;
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapBoxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapBoxC extends StatefulWidget {
+  TapBoxC({
+    Key? key,
+    this.active: false,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _TapBoxCState createState() => _TapBoxCState();
+}
+
+class _TapBoxCState extends State<TapBoxC> {
+  bool _highlight = false;
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+                  color: Colors.teal[700]!,
+                  width: 10.0,
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  TapBoxB({Key? key, this.active: false, required this.onChanged})
+      : super(key: key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
+      ),
+    );
+  }
 }
 
 class TapBoxA extends StatefulWidget {
-  TapBoxA({Key key}) : super(key: key);
+  TapBoxA({Key? key}) : super(key: key);
   @override
   _TapBoxAState createState() => _TapBoxAState();
 }
@@ -41,7 +182,7 @@ class _TapBoxAState extends State<TapBoxA> {
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppA extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -52,6 +193,40 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: TapBoxA(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyAppB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('flutter demo'),
+        ),
+        body: Center(
+          child: ParentWidgetB(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyAppC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('flutter demo'),
+        ),
+        body: Center(
+          child: ParentWidgetC(),
         ),
       ),
     );
